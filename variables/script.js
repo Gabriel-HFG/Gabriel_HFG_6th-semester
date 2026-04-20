50f905b2543250a77ff7525ba8f3196b0ca73a35
Gabriel Fimbres <gabitojuegos2@gmail.com>
2026-04-20 17:29:04 +0000

    buttons

---
+++ variables/script.js
@@ -0,0 +1,27 @@
+function function1() {
+    const firstName = "Gabriel";
+    const favoriteColor = "black";
+    let loc = "mexico";
+    let mood = "happy";
+    console.log(`i live in ${loc} my mood is ${mood}`);
+    loc = "chihuahua";
+    mood = "Super happy";
+    console.log(`i live in ${loc} my mood is ${mood}`);
+}
+function function2() {
+
+    let companyName = "Gabo. inc";
+    let foundingYear = 1999;
+    let isActive = true;
+    let fundingAmount = undefined;
+
+console.log(`Company name: ${companyName}
+Founding year: ${foundingYear}
+is Active: ${isActive}
+fundingAmount: ${fundingAmount}`);
+}
+function function3() {
+    let number = 5;
+    number = (((number * 2) + 10) / 2) - number;
+    alert(number);
+}
